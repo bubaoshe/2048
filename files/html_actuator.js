a4575b871276e76653d686af0a8f5c0a554409f3
bubaoshe <2561376271@qq.com>
2014-03-26 02:25:39 +0800

    utf8
    Switched to utf8 encoding

--- files/html_actuator.js
+++ files/html_actuator.js
@@ -52,7 +52,7 @@ HTMLActuator.prototype.clearContainer = function (container) {
 };
 
 HTMLActuator.prototype.addTile = function (tile) {
-  var text = "     ÌÆËÎÔªÃ÷Çå¡¡¡¡";
+  var text = "     ä¸ƒç³æœˆæ›¦å°é»‘ç™½";
   var self = this;
   var text2 = function (n) { var r = 0; while (n > 1) r++, n >>= 1; return r; }
 
